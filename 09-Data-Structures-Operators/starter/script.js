7f27363bab2844e9b6c13c3c0fa6f4f47980d1cf
Kapish Sharma <kapish@mindrops.com>
2021-11-18 13:17:38 +0530

    nvb

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,23 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -13,17 +30,208 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
   openingHours: {
-    thu: {
+    [weekdays[3]]: {
       open: 12,
       close: 22,
     },
-    fri: {
+    [weekdays[4]]: {
       open: 11,
       close: 23,
     },
-    sat: {
+    [weekdays[5]]: {
       open: 0, // Open 24 hours
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ address, starterIndex = 1, mainIndex = 2, time }) {
+    // alert(
+    //   `order recived! your ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
+    // );
+    console.log(
+      `order recived! your ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
+    );
+  },
+
+  // ES6 enhanced object litrel
+  // openingHours,
+  // order(starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  // orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+  //   console.log(
+  //     `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+  //   );
+  // },
+
+  // orderPasta(ing1, ing2, ing3) {
+  //   console.log(
+  //     `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+  //   );
+  // },
+
+  // orderPizza(mainIngredient, ...otherIngredients) {
+  //   console.log(mainIngredient);
+  //   console.log(otherIngredients);
+  // },
 };
+// destrcting objects
+
+// destrcting arrays
+
+// const [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// let [main, , secondary] = restaurant.categories;
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // nested Array
+// const nested = [45, 68, [75, 68]];
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // default values
+// const [p = 1, q = 2, r = 3] = [48, 75];
+// console.log(p, q, r);
+
+// restaurant.orderDelivery({
+//   address: prompt(`Enter Your Address`),
+//   time: prompt(`Enter time of delivery`),
+//   mainIndex: Number(prompt(`Enter index number`)),
+//   starterIndex: Number(prompt(`Enter index number`)),
+// });
+/*
+restaurant.orderDelivery({
+  address: '1274/87',
+  time: '5:30 PM',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const { name, categories, openingHours } = restaurant;
+console.log(name, categories, openingHours);
+
+const {
+  name: restaurantName,
+  categories: tags,
+  openingHours: hours,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// default values
+
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+// mutating variabels
+let a = 123;
+let b = 654;
+
+const obj = { a: 23, b: 85, c: 6 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested object
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+
+// coding chlalleg game
+
+// coding challenge 1
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// coding challeng answer
+
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+// // 3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+// // 4.
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+// // 5.
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+// // 6.
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
+// printGoals(...game.scored);
+// // 7.
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+////////////////
+// for-of loop
+////////////////
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// // for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}:${el}`);
+// }
